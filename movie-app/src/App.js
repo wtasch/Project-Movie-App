132a87baca90ae6c486c1cf89ac035857c550d02
ackottsi <kottsickandrewc@johndeere.com>
2021-02-19 15:11:03 -0600

    created movie-app.  updated App.js and verified ability to render to browswer

---
+++ movie-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React, { Component } from 'react';
+
+
+class App extends Component{
+
+  render(){
+  return (
+    <div className="App">
+      Testing
+    </div>
+  );
+}
+}
+
+export default App;
