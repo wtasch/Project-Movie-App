ce00d1c4fc519c7e88631f8ed5215731fa851fc8
HectorU <urdanetahector@johndeere.com>
2021-02-20 23:40:49 -0500

    added titles to each data tag in MovieDetail

--- movie-app/src/component/MovieDetail.js
+++ movie-app/src/component/MovieDetail.js
@@ -5,14 +5,18 @@ const MovieDetail = (props) => {
 
   return (
     <div>
-      <h3>{props.movieData[0].Title}</h3>
-      <h3>{props.movieData[0].Plot}</h3>
-      <h3>{props.movieData[0].Actors}</h3>
-      <h3>{props.movieData[0].Director}</h3>
-      <h3>{props.movieData[0].Genre}</h3>
-      <h3>{props.movieData[0].Released}</h3>
-      <h3>{props.movieData[0].Runtime}</h3>
-      <h3>{props.movieData[0].Poster}</h3>
+      <h2>{props.movieData[0].Title}</h2>
+      <h3>Summary</h3>
+      <p>{props.movieData[0].Plot}</p>
+      <h3>Actors/Cast</h3>
+      <p>{props.movieData[0].Actors}</p>
+      <h3>Director</h3>
+      <p>{props.movieData[0].Director}</p>
+      <h3>Genre:</h3><p>{props.movieData[0].Genre}</p>
+      <h3>Release Date:</h3><p>{props.movieData[0].Released}</p>
+      <h3>Runtime</h3><p>{props.movieData[0].Runtime}</p>
+      <h3>Poster URL:</h3>
+      <details><summary>{props.movieData[0].Poster}</summary></details>
 
 
 
