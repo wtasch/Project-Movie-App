8bc54390a1b107bc857ecc264d260d9656453422
HectorU <urdanetahector@johndeere.com>
2021-02-21 08:52:44 -0500

    implemented a foundMovie variable in MovieDetail

--- movie-app/src/component/MovieDetail.js
+++ movie-app/src/component/MovieDetail.js
@@ -3,24 +3,32 @@ import React from 'react';
 const MovieDetail = (props) => {
   console.log(props.movieData);
 
+  const foundMovie = props.movieData[0];
+
   return (
-    <div>
-      <h2>{props.movieData[0].Title}</h2>
-      <h3>Summary</h3>
-      <p>{props.movieData[0].Plot}</p>
-      <h3>Actors/Cast</h3>
-      <p>{props.movieData[0].Actors}</p>
-      <h3>Director</h3>
-      <p>{props.movieData[0].Director}</p>
-      <h3>Genre:</h3><p>{props.movieData[0].Genre}</p>
-      <h3>Release Date:</h3><p>{props.movieData[0].Released}</p>
-      <h3>Runtime</h3><p>{props.movieData[0].Runtime}</p>
-      <h3>Poster URL:</h3>
-      <details><summary>{props.movieData[0].Poster}</summary></details>
-
-
-
-    </div>
+      <div className = "summary">
+        <div>
+          <h2>{foundMovie.Title}</h2>
+          <h3>Summary</h3>
+          <p>{foundMovie.Plot}</p>
+        </div>
+        <div>
+          <h3>Actors/Cast</h3>
+          <p>{foundMovie.Actors}</p>
+        </div>
+        <div>  
+          <h3>Director</h3>
+          <p>{foundMovie.Director}</p>
+          <h3>Genre</h3><p>{foundMovie.Genre}</p>
+          <h3>Release Date</h3><p>{foundMovie.Released}</p>
+          <h3>Runtime</h3><p>{foundMovie.Runtime}</p>
+        </div> 
+        <div className="photoURL">
+          <h3>Poster URL:</h3>
+          <summary>{foundMovie.Poster}</summary>
+        </div>
+      </div>
+  
   )
 }
 
