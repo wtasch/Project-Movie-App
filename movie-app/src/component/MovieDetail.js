8a329f7be3aa43ccc7b7c529a0b467117b51d752
HectorU <urdanetahector@johndeere.com>
2021-02-21 08:58:18 -0500

    implemented ternary foundMovie in MovieDetail

--- movie-app/src/component/MovieDetail.js
+++ movie-app/src/component/MovieDetail.js
@@ -7,26 +7,32 @@ const MovieDetail = (props) => {
 
   return (
       <div className = "summary">
-        <div>
-          <h2>{foundMovie.Title}</h2>
-          <h3>Summary</h3>
-          <p>{foundMovie.Plot}</p>
-        </div>
-        <div>
-          <h3>Actors/Cast</h3>
-          <p>{foundMovie.Actors}</p>
-        </div>
-        <div>  
-          <h3>Director</h3>
-          <p>{foundMovie.Director}</p>
-          <h3>Genre</h3><p>{foundMovie.Genre}</p>
-          <h3>Release Date</h3><p>{foundMovie.Released}</p>
-          <h3>Runtime</h3><p>{foundMovie.Runtime}</p>
-        </div> 
-        <div className="photoURL">
-          <h3>Poster URL:</h3>
-          <summary>{foundMovie.Poster}</summary>
-        </div>
+        {foundMovie ? (
+          <div>
+            <div>
+              <h2>{foundMovie.Title}</h2>
+              <h3>Summary</h3>
+              <p>{foundMovie.Plot}</p>
+            </div>
+            <div>
+              <h3>Actors/Cast</h3>
+              <p>{foundMovie.Actors}</p>
+            </div>
+            <div>  
+              <h3>Director</h3>
+              <p>{foundMovie.Director}</p>
+              <h3>Genre</h3><p>{foundMovie.Genre}</p>
+              <h3>Release Date</h3><p>{foundMovie.Released}</p>
+              <h3>Runtime</h3><p>{foundMovie.Runtime}</p>
+            </div> 
+            <div className="photoURL">
+              <h3>Poster URL:</h3>
+              <summary>{foundMovie.Poster}</summary>
+            </div>
+          </div>  
+        ) :
+        <p>Movie detail not loaded</p>
+        }
       </div>
   
   )
