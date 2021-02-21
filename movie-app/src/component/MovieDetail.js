586b7f5861fffab433af468ffc23cccd1b80fcf1
HectorU <urdanetahector@johndeere.com>
2021-02-20 22:31:56 -0500

    verified initial data display on MovieDetail compoment

--- movie-app/src/component/MovieDetail.js
+++ movie-app/src/component/MovieDetail.js
@@ -2,9 +2,21 @@ import React from 'react';
 
 const MovieDetail = (props) => {
   console.log(props.movieData);
+
   return (
-    <h1>This is the MovieDetail</h1>
-    
+    <div>
+      <h3>{props.movieData[0].Title}</h3>
+      <h3>{props.movieData[0].Plot}</h3>
+      <h3>{props.movieData[0].Actors}</h3>
+      <h3>{props.movieData[0].Director}</h3>
+      <h3>{props.movieData[0].Genre}</h3>
+      <h3>{props.movieData[0].Released}</h3>
+      <h3>{props.movieData[0].Runtime}</h3>
+      <h3>{props.movieData[0].Poster}</h3>
+
+
+
+    </div>
   )
 }
 
