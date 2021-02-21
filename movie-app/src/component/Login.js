99d09d0704a662521746778271c4e4d76f9a4c0c
ackottsi <kottsickandrewc@johndeere.com>
2021-02-20 21:27:41 -0600

    created skeleton for login page

--- movie-app/src/component/Login.js
+++ movie-app/src/component/Login.js
@@ -3,16 +3,42 @@ import React, {Component} from 'react'
 class Login extends Component{
     constructor(props){
         super(props);
-        this
+        this.state={
+            username:'',
+            password:''
+        }
+    }
+
+    handleChange=(e)=>{
+        console.log(e.target)
+        const {username,password};
+        this.setState({
+            
+            [username]:value,
+            [password]:value
+        })
     }
 
     render(){
-    return(
-
-        <div>Login Page</div>
-
-
-         )
+        return(        
+            <form>
+                <label htmlFor="username">username</label>
+                <input 
+                    type="text"
+                    name="username"
+                    value={this.state.username}
+                    onChange={this.handleChange}
+                />
+                  <label htmlFor="password">Password</label>
+                <textarea
+                    type="password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.handleChange}
+                />
+                <input type="submit" value="Login!"/>
+            </form>
+        )
     }
 }
 export default Login;
