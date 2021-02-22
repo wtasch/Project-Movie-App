47333b9cf57dbe8ddecdaeeb5b99aeab51cb1eed
ericb2020 <ebrauer83@gmail.com>
2021-02-22 08:29:28 -0600

    add pictures to MovieList

--- movie-app/src/component/MovieList.js
+++ movie-app/src/component/MovieList.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../MovieList.css';
 
 const MovieList = (props) => {
   console.log(props);
   return (
-    <div> 
+    <div className="movieList"> 
       <h1>Movies!!.......</h1>
       {props.movieData.map(movie => (
-        <div key={movie.imdbID}>
-          <h3>
-            <Link to={`/MovieDetail/${movie.Title}`}>{movie.Title}</Link>
-          </h3>
+        <div key={movie.imdbID} className="movieCard">
+          <Link to={`/MovieDetail/${movie.Title}`}>
+            <div>
+              <img src={movie.Poster} alt={`${movie.Title} Poster`} />
+              {movie.Title}
+            </div>
+          </Link>
         </div>
       ))}
     </div>
