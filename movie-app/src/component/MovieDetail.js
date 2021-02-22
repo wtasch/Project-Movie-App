d1a63c7ae2f1fb2fdd6c573eeaf0f7df205206aa
HectorU <urdanetahector@johndeere.com>
2021-02-22 16:19:01 -0500

    initial styling of MovieDetail cocomponent

--- movie-app/src/component/MovieDetail.js
+++ movie-app/src/component/MovieDetail.js
@@ -17,29 +17,31 @@ const MovieDetail = (props) => {
             <div className="ratings">
               {foundMovie.Ratings.map((ratings, index) => 
                 <div key={index}>
-                  <p>{ratings.Source}</p>
-                  <p>{ratings.Value}</p>
+                  <h5>{ratings.Source}:</h5>
+                  <h5>{ratings.Value}</h5>
                 </div>
               )}
             </div>
-            <div className="poster">
-              <img src={foundMovie.Poster} alt="movie poster" />
-            </div>
-            <div className="plot">  
-              <p>{foundMovie.Plot}</p>
-            </div>
-            <div className="cast">
-              <h3>Actors/Cast</h3>
-              <p>{foundMovie.Actors}</p>
-            </div>
-            <div>  
-              <h3>Director</h3>
-              <p>{foundMovie.Director}</p>
+            <div className="wrapper">
+              <div className="poster">
+                <img src={foundMovie.Poster} alt="movie poster" />
+              </div>
+              <div className="plot">  
+                <h3>{foundMovie.Plot}</h3>
+            
+                <h3>Actors/Cast</h3>
+                <p>{foundMovie.Actors}</p>
+              
+                <h3>Director</h3>
+                <p>{foundMovie.Director}</p>
+                <h3>Languages</h3>
+                <p>{foundMovie.Language}</p>
+              </div>
             </div>
             <div className="various">  
-              <h3>Genre</h3><p>{foundMovie.Genre}</p>
-              <h3>Release Date</h3><p>{foundMovie.Released}</p>
-              <h3>Runtime</h3><p>{foundMovie.Runtime}</p>
+              <h5>Genre:</h5><h5>{foundMovie.Genre}</h5>
+              <h5>Release Date:</h5><h5>{foundMovie.Released}</h5>
+              <h5>Runtime:</h5><h5>{foundMovie.Runtime}</h5>
             </div>
             
              
