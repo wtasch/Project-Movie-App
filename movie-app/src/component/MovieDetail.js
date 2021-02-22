c0c90a3ac7672993a8edf23875768d793fb67b84
HectorU <urdanetahector@johndeere.com>
2021-02-22 12:40:51 -0500

    added google fonts to index.html

--- movie-app/src/component/MovieDetail.js
+++ movie-app/src/component/MovieDetail.js
@@ -11,33 +11,37 @@ const MovieDetail = (props) => {
       <div className = "movie-detail">
         {foundMovie ? (
           <div>
+            <div className="name-title">
+              <h2>{foundMovie.Title}</h2>
+            </div>
+            <div className="ratings">
+              {foundMovie.Ratings.map((ratings, index) => 
+                <div key={index}>
+                  <p>{ratings.Source}</p>
+                  <p>{ratings.Value}</p>
+                </div>
+              )}
+            </div>
             <div className="poster">
               <img src={foundMovie.Poster} alt="movie poster" />
             </div>
-            <div className="plot">
-              <h2>{foundMovie.Title}</h2>
-              <h3>Summary</h3>
+            <div className="plot">  
               <p>{foundMovie.Plot}</p>
             </div>
-            <div>
+            <div className="cast">
               <h3>Actors/Cast</h3>
               <p>{foundMovie.Actors}</p>
             </div>
             <div>  
               <h3>Director</h3>
               <p>{foundMovie.Director}</p>
+            </div>
+            <div className="various">  
               <h3>Genre</h3><p>{foundMovie.Genre}</p>
               <h3>Release Date</h3><p>{foundMovie.Released}</p>
               <h3>Runtime</h3><p>{foundMovie.Runtime}</p>
             </div>
-            <div>
-              {foundMovie.Ratings.map(ratings => 
-                <div>
-                  <p>{ratings.Source}</p>
-                  <p>{ratings.Value}</p>
-                </div>
-              )}
-            </div>
+            
              
             
           </div>  
