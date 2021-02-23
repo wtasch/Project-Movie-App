75dde81b34e24b3143964e79dee79ce940efafbb
wtasch <wtasch@aol.com>
2021-02-23 14:25:17 -0500

    added sloth title to news page

--- movie-app/src/component/Articles.js
+++ movie-app/src/component/Articles.js
@@ -7,6 +7,13 @@ const Articles = (props) => {
     console.log(props.news)
   return (
       <div className="news-container">
+
+<div className="newstitle">
+     
+        <h1 className="title6">Sloth</h1>
+        <h1 className="title7"> News</h1>
+      </div>
+
       <div className="article">
       {props.news.map(news => <p className="newstext" >{news.title}<img className="imgNews" src={news.urlToImage}/></p>)}  
           </div>
