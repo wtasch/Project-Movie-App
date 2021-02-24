92cd9c1a104371dfa72c1b35a46f115f84bb6a6b
HectorU <urdanetahector@johndeere.com>
2021-02-24 15:01:46 -0500

    added functionality to WatchList Component

--- movie-app/src/component/WatchList.js
+++ movie-app/src/component/WatchList.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import '../MovieList.css';
 
 const WatchList = (props) => {
   console.log(props);
+  const wishList = props.watchList;
+  console.log(wishList);
+  
+  
   return (
-    <h1>Watch List Page</h1>
+    <div className="movieList">
+        
+
+            {props.watchList.map(movie => (
+            <div key={movie.imdbID} className="movieCard">
+                <div className="movieCardImageContainer">
+                
+                    <img src={movie.Poster} alt={`${movie.Title} Poster`} />
+                
+                </div>
+                
+                <div className="movieCardTitle">
+                    {movie.Title}
+                </div>
+                
+            </div>
+            ))}
+       
+
+    </div>
+    
   )
 }
 
