ef8241da9540873888a647c202b0f3882bdea3ed
HectorU <urdanetahector@johndeere.com>
2021-02-24 14:00:31 -0500

    initial WatchList component created

---
+++ movie-app/src/component/WatchList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const WatchList = (props) => {
+  console.log(props);
+  return (
+    <h1>Watch List Page</h1>
+  )
+}
+
+export default WatchList;
